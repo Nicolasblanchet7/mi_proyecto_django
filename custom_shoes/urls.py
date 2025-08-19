a92f462d9c242966262aeba59857648dc1a2b632
Your Name <you@example.com>
2025-08-19 01:22:36 -0300

    Agregado el catalogo dinamico con imagenes

--- custom_shoes/urls.py
+++ custom_shoes/urls.py
@@ -1,9 +1,14 @@
+from django.urls import path
+from . import views
 
-# custom_shoes/urls.py
+urlpatterns = [
+    path('', views.index, name='index'),
+]
 
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:sneaker_name>/', views.product_detail, name='product_detail'),
 ]
--- custom_shoes/views.py
+++ custom_shoes/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Sneaker
 
 def index(request):
-    return render(request, 'custom_shoes/inicio.html')
+    all_sneakers = Sneaker.objects.all()
+    return render(request, 'custom_shoes/inicio.html', {'sneakers': all_sneakers})
+
+def product_detail(request, sneaker_name):  # <-- Asegúrate de que esta función esté aquí
+    sneaker = get_object_or_404(Sneaker, name=sneaker_name)
+    return render(request, 'custom_shoes/product_detail.html', {'sneaker': sneaker})
